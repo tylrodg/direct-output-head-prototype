e47e04a4d5a0a9ef5cae87ee60d21d640072dde2
Tyler Rodgers <tylrodg@u.northwestern.edu>
2020-03-12 21:51:07 -0500

    use port search from serial

--- pull/pull.py
+++ pull/pull.py
@@ -3,35 +3,17 @@ import glob
 import serial
 import time
 import re
+import os
+from serial.tools.list_ports import comports
 
 def serial_ports():
-    """ Lists serial port names
-
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/c*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-
-    logger_port = ''
-    for port in ports:
-        if 'usbmodem' in port:
-            try:
-                s = serial.Serial(port)
-                s.close()
-                logger_port = port
-            except (OSError, serial.SerialException):
-                pass
-    return logger_port
+    ports = comports()
+    port = ''
+    for p in ports:
+        if p.description == "Feather M0":
+            port = p.device
+            break
+    return port
 
 
 def get_files(port):
@@ -41,16 +23,14 @@ def get_files(port):
     ser = serial.Serial(port, 9600)
     ser.reset_input_buffer()
     ser.write(b"send files")
-    time.sleep(20)
+    time.sleep(10)
 
     while True:
         ser_bytes = ser.readline()
-        print(ser_bytes)
-        try: 
+        try:
             decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
         except:
             decoded_bytes = ''
-        print(decoded_bytes)
         if filename_regex.match(decoded_bytes):
             if decoded_bytes in filenames:
                 break
@@ -61,9 +41,30 @@ def get_files(port):
                 while row_regex.match(decoded_bytes):
                     f.write(decoded_bytes + '\n')
                     ser_bytes = ser.readline()
-                    decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
-        # with open("test_data.csv", "a") as f:
-        #     writer = csv.writer(f, delimiter=",")
-        #     writer.writerow([time.time(), decoded_bytes])
+                    decoded_bytes = ser_bytes[0:len(
+                        ser_bytes)-2].decode("utf-8")
+            print(filenames[-1] + ' successfully downloaded!')
+
+
 if __name__ == '__main__':
-    get_files(serial_ports())
+    print("Welcome to the Direct Output Head Data Retrieval Tool!")
+    port = ""
+    while not port:
+        port = input("Please enter the port name located in the bottom right corner of your Arduino IDE. If you would prefer for the program to search for it automatically, enter the word \"continue\" or enter \"quit\" to quit the program:\n").lower()
+        if port == "continue":
+            try:
+                get_files(serial_ports())
+            except:
+                print(
+                    "Sorry, the program was unable to find the appropriate port. Please try entering the port yourself.\n")
+                port = ""
+        elif port == "quit":
+            print("Thanks for using the Direct Output Head Data Retrieval Tool!")
+            sys.exit(0)
+        else:
+            try:
+                get_files(port)
+            except:
+                print(
+                    'That port was not accepted. Please try another port or allowing the program to search for a port.\n')
+                port = ""
